8ec1075d29bc524fb4f69bf38b029d5741924ce0
Cookey123 <129659519+Cookey123@users.noreply.github.com>
2026-09-08 21:08:30 +0200

    Add files via upload

---
+++ oppgave2c.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class oppgave2c {
+  public static void main(String[] args) {
+
+    Scanner tastatur = new Scanner(System.in);
+
+    for (int i = 1; i <= 10; i++) {
+
+      System.out.print("Skriv inn poengsum for student " + i + ": ");
+      int poeng = tastatur.nextInt();
+
+      while (poeng < 0 || poeng > 100) {
+        System.out.println("Ugyldig poengsum.");
+        System.out.print("Skriv inn poengsum på nytt: ");
+        poeng = tastatur.nextInt();
+      }
+
+      if (poeng >= 90) {
+        System.out.println("A");
+
+      } else if (poeng >= 80) {
+        System.out.println("B");
+
+      } else if (poeng >= 60) {
+        System.out.println("C");
+
+      } else if (poeng >= 50) {
+        System.out.println("D");
+
+      } else if (poeng >= 40) {
+        System.out.println("E");
+
+      } else {
+        System.out.println("F");
+      }
+    }
+  }
+}
---
+++ oppgave3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class oppgave3 {
+    public static void main(String[] args) {
+
+        Scanner tastatur = new Scanner(System.in);
+
+        System.out.print("Skriv inn et heltall større enn 0: ");
+        int n = tastatur.nextInt();
+
+        int fakultet = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fakultet = fakultet * i;
+        }
+
+        System.out.println(n + "! = " + fakultet);
+    }
+}
